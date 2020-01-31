aeb9d7c412594cce1831264b5ec31a9723ebe023
Julian Hess <jhess@broadinstitute.org>
2020-01-31 18:17:32 +0000

    Initial commit of Example 0.2

--- examples/0_basic_workflow.py
+++ examples/0_basic_workflow.py
@@ -5,21 +5,28 @@
 
 from wolf import Task, Workflow, output_helpers
 
-# ## Toy Workflow 1: a single task
+# Since wolF outputs workflow results to Pandas dataframes, we need to import Pandas as well to manipulate them.
+
+import pandas as pd
+pd.set_option('display.max_colwidth', -1)
+
+
+# ## Example 1: a single task
 #
 # Our first workflow will comprise a single task, which takes arrays of numbers and letters, concatenates them with a user-specifiable string, and writes them to a file.
-#
+
 # ### 1.1. Define the workflow
 
-# Workflows are defined as classes, which are derived from wolF's Workflow class.
-class Example(Workflow):
+# Workflows are defined as classes, which are children of wolF's Workflow class.
+class Example1(Workflow):
     
-    # The actual workflow is defined by the workflow() function inside this class.
-    # All parameters passed to the task (here, parameter_1) are specified in the
+    # The actual workflow is defined inside the workflow() method inside this class.
+    # All parameters passed to the workflow (here, parameter_1) are specified in the
     # function definition.
     def workflow(self, parameter_1):
         
-        # Tasks are defined as instances of wolF's task class.
+        # Tasks are defined as instances of wolF's task class. They must be defined as
+        # class properties (i.e., with self)
         self.task_A = Task(
             
           # Each task must have a unique name, which is used to identify the task
@@ -35,15 +42,18 @@ class Example(Workflow):
           #   2. (number = 1, letter = "b", p1 = parameter_1)
           #   ...
           #   6. (number = 5, letter = "ghi", p1 = parameter_1)
+          #
+          # Input parameter names are exported as shell variables accessible to the
+          # script below.
           inputs = {
-            "number" : [0,   1,   2,   3,   4,   5],
+            "number" : [0,   1,   2,   3,   4,    5],
             "letter" : ["a", "b", "c", "d", "ef", "ghi"],
             "p1" : parameter_1 
           },
             
           # Scripts are defined as an array of bash commands. 
-          # Here, we simply concatenate number/p1/letter and write to output.txt
-          # We also write p1 to output.abc
+          # Here, we simply concatenate $number/$p1/$letter and write to output.txt
+          # We also write $p1 to output.abc
           script = [
             "echo -n ${number}_${p1}_${letter} > output.txt",
             "echo -n ${p1} > output.abc"
@@ -65,7 +75,7 @@ class Example(Workflow):
 #
 # Now that we have defined our workflow, it's time to run it. Workflows are run in a context manager, which will automatically handle spinning up the cluster backend before the workflow starts and tearing it down after the workflow finishes.
 
-with Example(conf = { "compute_script" : "/usr/local/share/cga_pipeline/src/provision_worker_container_host.sh" }) as e:
+with Example1(conf = { "compute_script" : "/usr/local/share/cga_pipeline/src/provision_worker_container_host.sh" }) as e:
     e.run(parameter_1 = "foo", run_name = "foo_flow")
     e.run(parameter_1 = "bar", run_name = "bar_flow")
     e.run(parameter_1 = "baz", run_name = "baz_flow")
@@ -92,33 +102,36 @@ e.results
 #   
 # For example, let's look just at `foo_flow`'s outputs:
 
-# +
-import pandas as pd
-pd.set_option('display.max_colwidth', -1)
-
 e.results.loc["foo_flow", "outputs"]
-# -
 
-# Here are just the inputs for shard 5 of every workflow
+# Here are just the inputs for shard 5 of every workflow:
 
 e.results.loc[(slice(None), slice(None), "5"), "inputs"]
 
 
-class Example(Workflow):
+# It is very easy to interactively analyze the inputs and outputs of wolF workflows!
+
+# ## Example 2: multiple dependent tasks
+#
+# Our second workflow will comprise four tasks. The first task is the same as in [Example 1](#Example-1:-a-single-task).
+
+class Example2(Workflow):
     def workflow(self, parameter_1, run_task_C = True):
         self.task_A = Task(
           name = "print_the_string",
           inputs = {
-            "number" : [0,   1,   2,   3,   42,   1729],
+            "number" : [0,   1,   2,   3,   4,   5],
             "letter" : ["a", "b", "c", "d", "ef", "ghi"],
             "p1" : parameter_1 
           },
-          outputs = {
-            "number+p1+letter" : "*.txt"
-          },
           script = [
             "echo -n ${number}_${p1}_${letter} > output.txt",
-          ]
+            "echo -n ${p1} > output.abc"
+          ],
+          outputs = {
+            "number+p1+letter" : "output.txt",
+            "p1_only" : "*.abc"
+          }
         )
 
         self.task_B = Task(
@@ -167,21 +180,7 @@ class Example(Workflow):
           dependencies = [self.task_B, self.task_C]
         )
 
-        self.task_E = Task(
-          name = "docker_demo",
-          inputs = {
-            "dummy" : 0
-          },
-          outputs = {
-            "OS" : "*.txt"
-          },
-          script = [
-            "cat /etc/os-release > release.txt"
-          ],
-          docker = "debian"
-        )
-
-with Example() as e:
+with Example2() as e:
     e.run(parameter_1 = "foo", run_name = "foo_flow")
     e.run(parameter_1 = "bar", run_task_C = False, run_name = "bar_flow")
     e.run(parameter_1 = "baz", run_task_C = True, run_name = "baz_flow")
